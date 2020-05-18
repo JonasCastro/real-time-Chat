df2a4ff30a2860a9790f56ef0da6725fb78a3fdc
Jonas Castro <jonascastro128@gmail.com>
2020-05-17 23:10:42 -0300

    feat(front): private route configuration

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect, RouteComponentProps } from 'react-router-dom';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { SINGIN, CHATROOM } from './constants/routes';
+import { isAuthenticated } from './services/auth';
 import './styles/global.css';
+
+import { SINGIN, CHATROOM } from './constants/routes';
 
 import SignIn from './pages/SignIn';
 import ChatRoom from './pages/ChatRoom';
 
+interface Props {
+  component: React.FC<RouteComponentProps>;
+  exact?: boolean;
+  path: string;
+}
+const PrivateRoute = ({ component: Component, ...rest }: Props) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
+);
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path={SINGIN} component={SignIn} />
-      <Route path={CHATROOM} component={ChatRoom} />
+      <PrivateRoute path={CHATROOM} component={ChatRoom} />
     </Switch>
   </BrowserRouter>
 );
